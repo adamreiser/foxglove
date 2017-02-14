fe6a77f5caa3b67a6bf5897d5100cce20351e520
Adam Reiser <reiser@defensivecomputing.io>
2017-02-13 22:15:56 -0500

    style fixes

--- foxglove/__init__.py
+++ foxglove/__init__.py
@@ -1,7 +1,12 @@
-import os, sys, subprocess, atexit, logging
-import socket, argparse, tempfile
+import os
+import subprocess
+import atexit
+import socket
+import argparse
+import tempfile
 import mozprofile
 from shutil import copyfile
+
 
 def main():
     pkg_dir = os.path.split(os.path.abspath(__file__))[0]
@@ -15,37 +20,44 @@ def main():
         os.mkdir(os.path.join(work_dir, 'profiles'), 0o700)
 
     if not os.path.exists(os.path.join(work_dir, 'prefs.js')):
-        copyfile(os.path.join(pkg_dir, 'prefs.js'), \
-                os.path.join(work_dir, 'prefs.js'))
+        copyfile(os.path.join(pkg_dir, 'prefs.js'),
+                 os.path.join(work_dir, 'prefs.js'))
 
     ssh_dir = tempfile.mkdtemp()
 
-    parser = argparse.ArgumentParser(description='Manages Firefox proxy sessions.')
+    parser = argparse.ArgumentParser(
+            description='Manages Firefox proxy sessions.')
 
-    parser.add_argument('profile', type=str, help='The name of the profile to use')
+    parser.add_argument('profile',
+                        type=str, help='The name of the profile to use')
     parser.add_argument('host', type=str, help='The server to connect to')
 
     # TODO: port range
-    parser.add_argument('--port', type=int, metavar='N', nargs='?', default=0, help='The port to forward over (default: random)')
+    parser.add_argument('--port', type=int,
+                        metavar='N', nargs='?', default=0,
+                        help='The port to forward over (default: random)')
 
-    parser.add_argument('-d', action="store_true", default=False, help='Dry run (don\'t launch browser)')
-    parser.add_argument('--prefs', metavar='PATH', default=os.path.join(work_dir, 'prefs.js'), help="Path to the base preferences file (default: {})".format(os.path.join(work_dir, 'prefs.js')))
+    parser.add_argument('-d', action="store_true", default=False,
+                        help='Dry run (don\'t launch browser)')
+    parser.add_argument('--prefs', metavar='PATH',
+                        default=os.path.join(work_dir, 'prefs.js'),
+                        help="Path to the base preferences file (default: {})"
+                        .format(os.path.join(work_dir, 'prefs.js')))
 
     args = parser.parse_args()
 
     assert(os.path.exists(args.prefs))
 
     if args.port == 0:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(('127.0.0.1', args.port))
         args.port = s.getsockname()[1]
         s.close()
 
     cm_path = os.path.join(ssh_dir, args.profile + '_%r@%h:%p')
     ssh_base = ['ssh', '-S', cm_path, args.host]
-    cm_connect = ssh_base + ['-fNTM', '-D 127.0.0.1:' + str(args.port), \
-            '-o', 'ExitOnForwardFailure=yes']
-    cm_check = ssh_base + ['-O', 'check', args.host]
+    cm_connect = ssh_base + ['-fNTM', '-D 127.0.0.1:' + str(args.port),
+                             '-o', 'ExitOnForwardFailure=yes']
     cm_exit = ssh_base + ['-O', 'exit', args.host]
 
     # Exit functions run in reverse order
@@ -70,8 +82,9 @@ def main():
     prefs_dict.update({'network.proxy.socks_port': args.port})
 
     # Profile with updated preferences
-    mod_profile = mozprofile.FirefoxProfile(profile=profile_dir, \
-            preferences=prefs_dict)
+    mod_profile = mozprofile.FirefoxProfile(profile=profile_dir,
+                                            preferences=prefs_dict)
 
     if (not args.d):
-        subprocess.call(['firefox', '--new-instance', '--no-remote', '--profile', profile_dir])
+        subprocess.call(['firefox', '--new-instance',
+                         '--no-remote', '--profile', profile_dir])
