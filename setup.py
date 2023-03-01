9e6cda5dad48d97bfa07a84707c0e43ee21e6b99
Adam Reiser <reiser@defensivecomputing.io>
2023-02-28 19:06:44 -0800

    Re-add package_data

--- setup.py
+++ setup.py
@@ -84,6 +84,10 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
 
+    package_data={
+        'foxglove': ['prefs.js']
+    },
+
     include_package_data=True,
 
     # To provide executable scripts, use entry points in preference to the
